8b445428c4c608cba0ed4402ccd81e5bf63abf42
mqazi <ali.najeeb123@hotmail.com>
2016-04-25 18:45:18 +0000

    treading cautiously

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.order(:name)
   end
 
   # GET /users/1
@@ -25,28 +25,30 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+      format.html { redirect_to users_url,
+      notice: "User #{@user.name} was successfully created." }
+      format.json { render action: 'show',
+      status: :created, location: @user }
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors,
+        status: :unprocessable_entity }
       end
     end
   end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+  
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to users_url,
+        notice: "User #{@user.name} was successfully updated." }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors,
+        status: :unprocessable_entity }
       end
     end
   end
