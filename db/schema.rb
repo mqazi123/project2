c43498afbce4957771c83c9feaf2f2c23507e339
mqazi <ali.najeeb123@hotmail.com>
2016-04-25 18:29:18 +0000

    user scaffolding done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425174715) do
+ActiveRecord::Schema.define(version: 20160425182631) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20160425174715) do
     t.string   "help_needed"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
